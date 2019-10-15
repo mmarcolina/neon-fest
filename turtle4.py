52a13165fb6878f61459a4e87c29dce5e8381f39
Ryan McGovern <TekGadgt@users.noreply.github.com>
2019-10-15 19:30:38 -0400

    Comments for use at NeonFest

--- turtle4.py
+++ turtle4.py
@@ -1,20 +1,20 @@
 import turtle
 import random
 
-turtle.bgcolor("black")
-color = ["red", "orange", "yellow", "light green", "cyan", "pink"]
+turtle.bgcolor("black")                                             #The Background Color of the Turtle Window
+color = ["red", "orange", "yellow", "light green", "cyan", "pink"]  #The Various Colors Available to Turtle
 
-for i in range(4):
+for i in range(4):                                                  #The Number of Times that Turtle Moves
     turtle.pencolor(random.choice(color))
-    turtle.forward(100)
-    turtle.right(50)
-    for i in range(3):
+    turtle.forward(100)                                             #The Length that Turtle Moves
+    turtle.right(50)                                                #The Angle that Turtle Turns
+    for i in range(3):                                              #The Number of Times that Turtle Moves
         turtle.pencolor(random.choice(color))
-        turtle.forward(100)
-        turtle.right(120)
-        for i in range(8):
+        turtle.forward(100)                                         #The Length that Turtle Moves
+        turtle.right(120)                                           #The Angle that Turtle Turns
+        for i in range(8):                                          #The Number of Times that Turtle Moves
             turtle.pencolor(random.choice(color))
-            turtle.forward(100)
-            turtle.right(45)
+            turtle.forward(100)                                     #The Length that Turtle Moves
+            turtle.right(45)                                        #The Angle that Turtle Turns
 
 turtle.mainloop()
