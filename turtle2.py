9a6a3cb9b66ec1779927618ee14c227e331bcbec
Ryan McGovern <TekGadgt@users.noreply.github.com>
2019-10-15 19:18:35 -0400

    Comments for use at NeonFest

--- turtle2.py
+++ turtle2.py
@@ -1,8 +1,8 @@
 import turtle
 
-for i in range(4):
-    turtle.forward(150)
-    turtle.right(90)
-    turtle.circle(50)
+for i in range(4):          #The Number of Times Turtle Moves
+    turtle.forward(150)     #The Length that Turtle Moves
+    turtle.right(90)        #The Angle that Turtle Turns
+    turtle.circle(50)       #The Size of the Circle that Turtle makes
 
 turtle.mainloop()
