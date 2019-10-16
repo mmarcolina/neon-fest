149f09993d95c1d33da4c2b0987555702c7fb031
Mason Marcolina <40670829+mmarcolina@users.noreply.github.com>
2019-10-16 09:44:47 -0400

    Updated comments
    Removed some comment redundancy to make clearer what the nested for-loop accomplishes.

--- turtle6.py
+++ turtle6.py
@@ -8,12 +8,12 @@ screen = Screen()
 screen.bgcolor("black")                                             #The Background Color of the Turtle Window
 
 for x in range(360):                                                #The Number of Times that Turtle Moves
-    pen.pencolor(colors[x%6])  
+    pen.pencolor(colors[x%6])                                       #Chooses a new color each time the loop starts
     pen.width(x/100 + 1)                                            #Determines the Width of Turtle, Increases Every Run
     pen.forward(x)                                                  #The Length that Turtle Moves, Increases Every Run
     pen.left(59)                                                    #The Angle that Turtle Turns
-    for j in range(4):                                              #The Number of Times that Turtle Moves
-        pen.forward(5)                                              #The Length that Turtle Moves
-        pen.right(90)                                               #The Angle that Turtle Turns
+    for j in range(4):                                              #Creates a square after the main loop finishes
+        pen.forward(5)
+        pen.right(90)
 
 screen.exitonclick()    
